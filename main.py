37c558239a3448ba3077124877d42e57fbdc5480
sauravnav <86315988+sauravnav@users.noreply.github.com>
2021-06-26 15:31:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+l = [1, 3, 5,7]
+n = int(input("enter numbers of next odd number you want to print:"))
+d = l[-1]  #7
+for i in range(n):           
+    e = d+2                #7+2
+    l.append(e)      #9
+    d = l[-1+i]        #11
+print(l)
